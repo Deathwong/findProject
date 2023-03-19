14c6d8dbf65e682abcab9e44090aa3268bfa12e8
borenoitg <daniel.anani@outlook.com>
2023-03-19 15:09:15 +0100

    Gestion du formulaire ajax sur l'écran d'accueil

--- views/index.php
+++ views/index.php
@@ -35,45 +35,6 @@ controller();
         </table>
     </div>
 </div>
-<!--<div>-->
-<!--    <div>--><?php //= $user->getUseId() ?><!--</div>-->
-<!--    <div>--><?php //= $user->getUseEmail() ?><!--</div>-->
-<!--    <div>--><?php //= $user->getUsePassword() ?><!--</div>-->
-<!--</div>-->
-<!--<div>-->
-<!--    <form action="">-->
-<!--        <div>-->
-<!--            <label for="nom">nom</label>-->
-<!--            <input type="text" id="nom" name="nom">-->
-<!--        </div>-->
-<!--        <div>-->
-<!--            <label for="prix_min">Prix min</label>-->
-<!--            <input type="text" id="prix_min" name="prix_min">-->
-<!--        </div>-->
-<!--        <div>-->
-<!--            <label for="prix_max">Prix max</label>-->
-<!--            <input type="text" id="prix_max" name="prix_max">-->
-<!--        </div>-->
-<!--        <div>-->
-<!--            <label for="categorie_id"></label>-->
-<!--            <select name="categorieId" id="categorie_id">-->
-<!--                <option>---Catégories---</option>-->
-<!--                --><?php
-//                foreach ($categories as $category) {
-//                    $categoryLibelle = $category->getCatLibelle();
-//                    $categoryId = $category->getCatId();
-//                    ?>
-<!--                    <option value="--><?php //= $categoryId ?><!--">--><?php //= $categoryLibelle ?><!--</option>-->
-<!--                    --><?php
-//                }
-//                ?>
-<!--            </select>-->
-<!--        </div>-->
-<!--    </form>-->
-<!--    <button onclick="rechercheAjax()">Rechercher</button>-->
-<!--</div>-->
-<!---->
-
 <?php require 'footer.php' ?>
 </body>
 </html>
