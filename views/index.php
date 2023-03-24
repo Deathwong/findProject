a4fb7e491ac034a3ca268f6cf3a695c10668f7f0
Deathwong <97117818+Deathwong@users.noreply.github.com>
2023-03-24 14:31:46 +0100

    Ihm (#8)
    * Ajout du navbar et du footer
    
    * Gestion du formulaire ajax sur l'écran d'accueil
    
    * Fix formulaire
    
    * Fix formulaire
    
    * gestion du login
    
    * Rajout du menu sur tous les écrans
    
    * fix nav bar
    
    * mise en place des cards
    
    * mise en place des cards
    
    * mise en place des cards
    
    ---------
    
    Co-authored-by: borenoitg <daniel.anani@outlook.com>

--- views/index.php
+++ views/index.php
@@ -11,30 +11,6 @@ controller();
 <div class="container">
     <div>
         <h1>Liste Annonces</h1>
-        <!--        <table id="annonce-tab">-->
-        <!--            <tr>-->
-        <!--                <th>Photo</th>-->
-        <!--                <th>Titre</th>-->
-        <!--                <th>Prix</th>-->
-        <!--                <th>Description</th>-->
-        <!--            </tr>-->
-        <!---->
-        <!--            --><?php
-        //            foreach ($annonces as $annonce) {
-        //                ?>
-        <!--                <tr>-->
-        <!--                    <td>-->
-        <!--                        <img src="../assets/img/annonces/-->
-        <?php //= $annonce->getAnnPhoto() ?><!--" alt="image de l'annonce">-->
-        <!--                    </td>-->
-        <!--                    <td>--><?php //= $annonce->getAnnNom() ?><!--</td>-->
-        <!--                    <td>--><?php //= $annonce->getAnnPrix() ?><!--</td>-->
-        <!--                    <td>--><?php //= $annonce->getAnnDescription() ?><!--</td>-->
-        <!--                </tr>-->
-        <!--                --><?php
-        //            }
-        //            ?>
-        <!--        </table>-->
 
         <div class="row row-cols-1 row-cols-md-3 g-4" id="annonce-cards-grid">
             <?php foreach ($annonces as $annonce) { ?>
@@ -45,8 +21,11 @@ controller();
                              alt="Skyscrapers"/>
 
                         <div class="card-body">
+                            <div class="card-title">
 
-                            <h5 class="card-title"><?= $annonce->getAnnNom() ?></h5>
+                                <h5><?= $annonce->getAnnNom() ?></h5>
+                                <h5><?= $annonce->getAnnPrix() ?> €</h5>
+                            </div>
 
                             <p class="card-text">
                                 <?= $annonce->getAnnDescription() ?>
@@ -56,11 +35,7 @@ controller();
                         <div class="card-footer">
                             <!--Détails annonce-->
                             <a href="detailsAnnonce.php?idAnnonce=<?= $annonce->getAnnId() ?>"
-                               class="btn btn-light icon-hover px-2 pt-2"><i class="fa-sharp fa-solid fa-info"></i></a>
-
-                            <!--Détails Favorie-->
-                            <input type="checkbox" id="favori" name="favori" aria-label="favoris"
-                                   onchange="addOrRemoveFavori(<?= $annonce->getAnnId() ?>)">
+                               class="btn btn-primary shadow-0 me-1">Détails</a>
                         </div>
                     </div>
                 </div>
