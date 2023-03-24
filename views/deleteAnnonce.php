04d83e438cebd338f1467bfbd4c13035a2cf68b6
darky33 <joanendg@gmail.com>
2023-03-24 15:11:52 +0100

    Suppression du script sur la page deleteAnnonce.php

--- views/deleteAnnonce.php
+++ views/deleteAnnonce.php
@@ -1,21 +1,3 @@
 <?php
 require '../Controller/AppController.php';
 controller();
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<?php require 'header.php' ?>
-<body>
-<div>
-    Votre annonce est suppimée.
-</div>
-<script>
-    // On récupère l'id du user connecté
-    userConnectId = <?=  $user->getUseId() ?>;
-    // Récupération de l'id du créateur de l'annonce
-    userAnnonceId = <?= $annonce->getUseId() ?>;
-    // Vérification de l'accès à la page de suppresion
-    isUserAuthorizedToDelete(userConnectId, userAnnonceId)
-
-</script>
-</body>
